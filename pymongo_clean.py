65a09523c6b61639b1361a7dd7048613a8f30375
chiziheng <40351172+chiziheng@users.noreply.github.com>
2020-11-15 19:50:58 +0800

    Update pymongo_clean.py

--- pymongo_clean.py
+++ pymongo_clean.py
@@ -1,15 +1,25 @@
 import pymongo
-myclient = pymongo.MongoClient("mongodb://35.174.211.255:27017/")
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["kindle_metadata"]
 mycol = mydb["kindle_metadata_origin"]
 mycol_origin = mydb["meta_Kindle_Store"]
 
 def update(x):
   myquery = { "asin": x['asin'] }
+  # print(x['asin'])
+  title = mycol_origin.find({'asin':x['asin']})[0]['title']
   newvalues = { "$set": { "title": x['title']} }
   mycol.update_one(myquery, newvalues, upsert=True)
 
-for documents in mycol_origin.find():
-  # print(documents['title'])
-   update(documents)
-   print('finish one')
+i=0
+ls_failed=[]
+for documents in mycol.find({"title":{"$exists":False}}):
+  try:
+    update(documents)
+    print(documents['asin'], 'inserted')
+  except:
+    print('cannot find record', documents['asin'])
+    ls_failed.append(documents['asin'])
+
+  i+=1
+  print('finish record', i)
