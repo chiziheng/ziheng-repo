2721bdbd83f4bacab130ec6323de5574859610ea
chiziheng <40351172+chiziheng@users.noreply.github.com>
2020-11-18 18:30:10 +0800

    Update mongo_commands.js

--- mongo_commands.js
+++ mongo_commands.js
@@ -1,5 +1,5 @@
 use kindle_metadata
 db.createCollection("kindle_metadata")
 use web_logs
-df.createCollection("web_logs")
+db.createCollection("web_logs")
 db.createUser({user:'Mongodbadmin',pwd:'Mongodbadmin',roles:[{role:'readWrite',db:'kindle_metadata'},{role:'readWrite',db:'web_logs'}]})
