68b8da78db4d248a62859f874d66426717f326b3
chiziheng <40351172+chiziheng@users.noreply.github.com>
2020-11-18 23:24:10 +0800

    Update MongoDB setup script.sh

--- MongoDB setup script.sh
+++ MongoDB setup script.sh
@@ -1,5 +1,5 @@
 echo "Downloading kindle metadata"
-sudo apt update
+sudo apt-get update
 sudo apt install unzip
 #wget -c https://istd50043.s3-ap-southeast-1.amazonaws.com/meta_kindle_store.zip -O meta_kindle_store.zip
 wget -c https://github.com/chiziheng/ziheng-s-first-repo/blob/main/kindle_metadata_final.zip?raw=true -O kindle_metadata_final.zip
